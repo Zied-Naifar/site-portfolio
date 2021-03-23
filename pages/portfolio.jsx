25ccde73aaef8c55263147d63cdea9f7d87920f8
Zied-Naifar <47352503+Zied-Naifar@users.noreply.github.com>
2021-03-23 09:05:16 +0100

    images

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -20,16 +20,100 @@ function Portfolio() {
 			      </p>
 
 			      <div className="portfolio-wrapper">
-			      	<div className="portfolio-item">
-			      		<img src="/chitchat.png" className="portfolio-image" />
+				  <div className="portfolio-item">
+			      		<img src="/1.jpg" className="portfolio-image" />
 
 			      		<h4 className="portfolio-name">Chat App Exploration</h4>
 			      		<div className="portfolio-category">Mobile Dev</div>
 			      	</div>
 			      	<div className="portfolio-item">
-			      		<img src="/kajian.jpg" className="portfolio-image" />
+			      		<img src="/2.jpg" className="portfolio-image" />
 
-			      		<h4 className="portfolio-name">kajian App</h4>
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/3.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/4.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/5.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/6.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/7.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/8.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/9.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/10.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/11.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/12.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/13.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/14.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/15.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
+			      		<div className="portfolio-category">Mobile Dev</div>
+			      	</div>
+					  <div className="portfolio-item">
+			      		<img src="/16.jpg" className="portfolio-image" />
+
+			      		<h4 className="portfolio-name">Chat App Exploration</h4>
 			      		<div className="portfolio-category">Mobile Dev</div>
 			      	</div>
 			      </div>
